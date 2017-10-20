27fcec970e59edaccfcbdf2f222a011edd921f76
Timo Schilling <timo@schilling.io>
2017-10-20 22:51:04 +0200

    dynamic accessories size

--- fritzbridge.go
+++ fritzbridge.go
@@ -7,30 +7,30 @@ import (
 )
 
 func main() {
-  var accessories [3]*accessory.Accessory
+  accessories := []*accessory.Accessory{}
 
   info := accessory.Info{
     Name:         "Fritz!Bridge",
     Manufacturer: "Timo Schilling",
   }
 
-  accessories[0] = accessory.NewThermostat(info, 20, 16, 28, 0.5).Accessory
+  accessories = append(accessories, accessory.NewThermostat(info, 20, 16, 28, 0.5).Accessory)
 
   info2 := accessory.Info{
     Name:         "Room 2",
     Manufacturer: "Timo Schilling",
   }
 
-  accessories[1] = accessory.NewThermostat(info2, 20, 16, 28, 0.5).Accessory
+  accessories = append(accessories, accessory.NewThermostat(info2, 20, 16, 28, 0.5).Accessory)
 
   info3 := accessory.Info{
     Name:         "Room 3",
     Manufacturer: "Timo Schilling",
   }
 
-  accessories[2] = accessory.NewThermostat(info3, 20, 16, 28, 0.5).Accessory
+  accessories = append(accessories, accessory.NewThermostat(info3, 20, 16, 28, 0.5).Accessory)
 
-  t, err := hc.NewIPTransport(hc.Config{Pin: "12341234", StoragePath: "database"}, accessories[0], accessories[1], accessories[2])
+  t, err := hc.NewIPTransport(hc.Config{Pin: "12341234", StoragePath: "database"}, accessories[0], accessories[1:len(accessories)]...)
   if err != nil {
     log.Fatal(err)
   }
