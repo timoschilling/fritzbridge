75bba796a8f2120300f9ccd4995929ae340ce645
Timo Schilling <timo@schilling.io>
2017-10-20 23:01:32 +0200

    loop it

--- fritzbridge.go
+++ fritzbridge.go
@@ -4,31 +4,20 @@ import (
   "github.com/brutella/hc"
   "github.com/brutella/hc/accessory"
   "log"
+  "fmt"
 )
 
 func main() {
   accessories := []*accessory.Accessory{}
 
-  info := accessory.Info{
-    Name:         "Fritz!Bridge",
-    Manufacturer: "Timo Schilling",
+  for i := 0; i < 3; i++ {
+    info := accessory.Info{
+      Name:         fmt.Sprintf("Room %d", i + 1),
+      Manufacturer: "Timo Schilling",
+    }
+
+    accessories = append(accessories, accessory.NewThermostat(info, 20, 16, 28, 0.5).Accessory)
   }
-
-  accessories = append(accessories, accessory.NewThermostat(info, 20, 16, 28, 0.5).Accessory)
-
-  info2 := accessory.Info{
-    Name:         "Room 2",
-    Manufacturer: "Timo Schilling",
-  }
-
-  accessories = append(accessories, accessory.NewThermostat(info2, 20, 16, 28, 0.5).Accessory)
-
-  info3 := accessory.Info{
-    Name:         "Room 3",
-    Manufacturer: "Timo Schilling",
-  }
-
-  accessories = append(accessories, accessory.NewThermostat(info3, 20, 16, 28, 0.5).Accessory)
 
   t, err := hc.NewIPTransport(hc.Config{Pin: "12341234", StoragePath: "database"}, accessories[0], accessories[1:len(accessories)]...)
   if err != nil {
